5907d268fc382d447433a9ec43795d7864114735
Guido Menendez <guidomenendez0@gmail.com>
2024-10-13 09:10:26 -0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import {errorMiddleware} from "./api/errors/handling/ErrorHandler";
 import {PORT} from "./utils/config";
 import {logger} from "./utils/container";
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(errorMiddleware);
 
@@ -11,9 +13,8 @@ app.listen(PORT, () => {
     logger.logInfo(`Server is running on port ${PORT}`);
 });
 
-
 //TODO:
 //0. Levantar envVars para envio de mails
 //0.1. Mapear errores a statusCodes
-//1. Codear POST /v1/event
+//1. Codear POST /v1/eventNotification
 //2. Probar POST /v1/event
