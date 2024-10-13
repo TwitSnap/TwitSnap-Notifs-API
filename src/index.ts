b51d6472da72b0501f8ebd2e7ebdb76087b33874
Guido Menendez <guidomenendez0@gmail.com>
2024-10-12 21:09:01 -0300

    Add to-dos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 console.log('Happy developing ✨');
+
+//TODO:
+//1. Codear EmailNotificationStrategy.sendNotification()
+//2. Codear RegistrationEvent.asString()
+//3. Codear EventService.createEvent()
+//4. Codear NotifyService.notifyEvent()
+//5. Codear POST /v1/event
+//6. Probar POST /v1/event
